182fc7e5130f3093b3ca9494dd87130367676aef
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-03-06 21:04:42 +0300

    Create a new empty activity project in Android Studio.

---
+++ app/src/main/java/com/vasilevkin/fragmentsroom/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vasilevkin.fragmentsroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
