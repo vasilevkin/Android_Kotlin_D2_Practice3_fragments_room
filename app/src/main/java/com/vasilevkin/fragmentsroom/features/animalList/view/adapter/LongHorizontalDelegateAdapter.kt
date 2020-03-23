d3b978b11e6cf1561b2f9948e1f8b6b320c444af
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-03-23 14:23:41 +0300

    Implement setOnClickListener for Square and long horizontal item.

--- app/src/main/java/com/vasilevkin/fragmentsroom/features/animalList/view/adapter/LongHorizontalDelegateAdapter.kt
+++ app/src/main/java/com/vasilevkin/fragmentsroom/features/animalList/view/adapter/LongHorizontalDelegateAdapter.kt
@@ -2,8 +2,10 @@ package com.vasilevkin.fragmentsroom.features.animalList.view.adapter
 
 import com.vasilevkin.fragmentsroom.R
 import com.vasilevkin.fragmentsroom.delegateadapter.KDelegateAdapter
-import com.vasilevkin.fragmentsroom.utils.downloadImageInView
+import com.vasilevkin.fragmentsroom.features.animalList.view.ui.AnimalListFragment
+import com.vasilevkin.fragmentsroom.models.localModels.Animal
 import com.vasilevkin.fragmentsroom.models.localModels.LongHorizontalCatLocalModel
+import com.vasilevkin.fragmentsroom.utils.downloadImageInView
 import kotlinx.android.synthetic.main.long_horizontal_item.*
 import kotlinx.android.synthetic.main.long_horizontal_item.view.*
 
@@ -12,9 +14,13 @@ class LongHorizontalDelegateAdapter : KDelegateAdapter<LongHorizontalCatLocalMod
 
     override fun onBind(item: LongHorizontalCatLocalModel, viewHolder: KViewHolder) =
         with(viewHolder) {
-            title_text_view.text = item.title
-            subtitle_text_view.text = item.subtitle
-            downloadImageInView(item.context, itemView.details_image, item.imageUrl)
+            title_text_view.text = item.animal.title
+            subtitle_text_view.text = item.animal.subtitle
+            downloadImageInView(item.context, itemView.details_image, item.animal.imageUrl.orEmpty())
+            itemView.setOnClickListener {
+                val act = item.context as AnimalListFragment.OnAnimalSelected
+                act.onSelected(Animal(item.animal.title))
+            }
         }
 
     override fun isForViewType(items: List<*>, position: Int) =
--- app/src/main/java/com/vasilevkin/fragmentsroom/features/animalList/view/adapter/SquareDelegateAdapter.kt
+++ app/src/main/java/com/vasilevkin/fragmentsroom/features/animalList/view/adapter/SquareDelegateAdapter.kt
@@ -2,6 +2,8 @@ package com.vasilevkin.fragmentsroom.features.animalList.view.adapter
 
 import com.vasilevkin.fragmentsroom.R
 import com.vasilevkin.fragmentsroom.delegateadapter.KDelegateAdapter
+import com.vasilevkin.fragmentsroom.features.animalList.view.ui.AnimalListFragment
+import com.vasilevkin.fragmentsroom.models.localModels.Animal
 import com.vasilevkin.fragmentsroom.models.localModels.SquareCatLocalModel
 import com.vasilevkin.fragmentsroom.utils.downloadImageInView
 import kotlinx.android.synthetic.main.square_item.*
@@ -15,6 +17,10 @@ class SquareDelegateAdapter : KDelegateAdapter<SquareCatLocalModel>() {
             title_text_view.text = item.animal.title
             subtitle_text_view.text = item.animal.subtitle
             downloadImageInView(item.context, itemView.details_image, item.animal.imageUrl.orEmpty())
+            itemView.setOnClickListener {
+                val act = item.context as AnimalListFragment.OnAnimalSelected
+                act.onSelected(Animal(item.animal.title))
+            }
         }
 
     override fun isForViewType(items: List<*>, position: Int) =
